15cf8a32140512eb25631538dfaecddbac4c0fed
Francisco Marquez <fmarquezestrada@gmail.com>
2019-06-16 11:03:34 -0500

    Primer commit -colas

---
+++ server/sockets/socket.js
@@ -0,0 +1,77 @@
+const { io } = require('../server');
+
+
+const {TicketControl} = require('../classes/ticket-control');
+
+const ticketControl = new TicketControl();
+
+io.on('connection', (client) => {
+
+
+    client.on('siguienteTicket',(data,callback)=>{
+        let sigTicket = ticketControl.siguiente();
+        console.log(sigTicket);
+        callback(sigTicket);
+    })
+
+
+   
+    client.emit('estadoActual',{
+        actual:ticketControl.getUltimoTicket(),
+        ultimos4: ticketControl.getUltimos4()
+    })
+
+    client.on('atenderTicket',(data,callback)=>{
+        if(!data.escritorio){
+            return callback({err:true, message:'El escritorio es necesario be'})
+        }
+
+        let atenderTicket= ticketControl.atenderTicket(data.escritorio);
+
+        callback(atenderTicket);
+
+        client.broadcast.emit('ultimos4', {            
+        ultimos4 : ticketControl.getUltimos4()
+        });
+
+    })
+
+
+    
+    // console.log('Usuario conectado');
+
+    // client.emit('enviarMensaje', {
+    //     usuario: 'Administrador',
+    //     mensaje: 'Bienvenido a esta aplicación'
+    // });
+
+
+
+    // client.on('disconnect', () => {
+    //     console.log('Usuario desconectado');
+    // });
+
+    // // Escuchar el cliente
+    // client.on('enviarMensaje', (data, callback) => {
+
+    //     console.log(data);
+
+    //     client.broadcast.emit('enviarMensaje', data);
+
+
+    //     // if (mensaje.usuario) {
+    //     //     callback({
+    //     //         resp: 'TODO SALIO BIEN!'
+    //     //     });
+
+    //     // } else {
+    //     //     callback({
+    //     //         resp: 'TODO SALIO MAL!!!!!!!!'
+    //     //     });
+    //     // }
+
+
+
+    // });
+
+});
